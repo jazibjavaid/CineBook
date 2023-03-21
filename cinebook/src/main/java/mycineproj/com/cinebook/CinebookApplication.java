205c595935f7dae35a32466485c3ef8e4ef06f13
jazibjavaid <javaid.j@northeastern.edu>
2023-03-20 20:34:06 -0400

    Created project structure and basic GET requests

---
+++ cinebook/src/main/java/mycineproj/com/cinebook/CinebookApplication.java
@@ -0,0 +1,15 @@
+package mycineproj.com.cinebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class CinebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinebookApplication.class, args);
+	}
+
+}
